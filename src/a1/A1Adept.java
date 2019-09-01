bd5f73b8f6ed5bdbacf2986d74dbea32f50c3bde
fenge <fenge@DESKTOP-8B9A7RH>
2019-08-31 21:34:50 -0400

    A1 Adept

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -1,6 +1,7 @@
 package a1;
 
 import java.util.Scanner;
+import java.util.Hashtable;
 
 public class A1Adept {
 
@@ -10,5 +11,43 @@ public class A1Adept {
 
 		// Your code follows here.
 		
+		Hashtable<String, Double> prices = new Hashtable<String, Double>();
+		int numStoreItems = scan.nextInt();
+		for (int i = 0; i < numStoreItems; i++) {
+			prices.put(scan.next(), scan.nextDouble());
+		}
+		
+		int numCustomers = scan.nextInt();
+		double totalSpent = 0;
+		String highestCustomer = "";
+		double highestSpent = 0;
+		String lowestCustomer = "";
+		double lowestSpent = Double.POSITIVE_INFINITY;
+		
+		for (int i = 0; i < numCustomers; i++) {
+			String currentCustomer = scan.next() + " " + scan.next();
+			double currentSpent = 0;
+			int numPurchases = scan.nextInt();
+			
+			for (int j = 0; j < numPurchases; j++) {
+				currentSpent += scan.nextInt() * prices.get(scan.next());
+			}
+			totalSpent += currentSpent;
+			
+			if (currentSpent > highestSpent) {
+				highestCustomer = currentCustomer;
+				highestSpent = currentSpent;
+			}
+			if (currentSpent < lowestSpent) {
+				lowestCustomer = currentCustomer;
+				lowestSpent = currentSpent;
+			}
+		}
+		
+		System.out.printf("Biggest: %s (%.2f)%n", highestCustomer, highestSpent);
+		System.out.printf("Smallest: %s (%.2f)%n", lowestCustomer, lowestSpent);
+		System.out.printf("Average: %.2f", totalSpent/numCustomers);
+		
+		scan.close();
 	}
 }
